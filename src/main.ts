296992c8e2cc321a89b49b4ec67eb66e46868de8
Hadi Haidar <hadi12121211@gmail.com>
2025-11-04 10:29:16 +0200

    add admin-pharmacy web to the cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Enable CORS for your React app
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://admin-pharmacy-orpin.vercel.app'], // your Vite React app URLs
+    credentials: true,               // if you're using cookies or auth headers
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
